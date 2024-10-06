72f7ca4f0f35084071a23fff03116c3969c3e00b
rcy <21010078@mail.ecust.edu.cn>
2024-10-06 22:05:12 +0800

    init

---
+++ llm_access/LLM.py
@@ -0,0 +1,32 @@
+from config.get_config import config_data
+from llm_access import get_api
+
+def get_llm():
+    model_provider = config_data['llm']['model_provider']
+    if model_provider == "qwen":
+        from langchain_community.llms import Tongyi
+
+        llm = Tongyi(dashscope_api_key=get_api.get_api_key_from_file(),
+                     model_name=config_data['llm']['model'])
+
+        # path = r'D:\IDLE\big\qwen\models\Qwen-1_8B-Chat'
+        # from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+        # from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+        # # pip install tiktoken torch
+        # # pip install transformers_stream_generator einops
+        # # pip install accelerate
+        # tokenizer = AutoTokenizer.from_pretrained(path, revision='master', trust_remote_code=True)
+        # model = AutoModelForCausalLM.from_pretrained(path, revision='master', device_map="auto", trust_remote_code=True)
+        # pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10)
+        # llm = HuggingFacePipeline(pipeline=pipe)
+
+    else:
+        from langchain_openai import ChatOpenAI
+
+        llm = ChatOpenAI(
+            temperature=0.95,
+            model=config_data['llm']['model'],
+            openai_api_key=get_api.get_api_key_from_file("./llm_access/api_key_openai.txt"),
+            openai_api_base=config_data['llm']['url'],
+        )
+    return llm
---
+++ llm_access/call_llm_test.py
@@ -0,0 +1,16 @@
+from langchain.chains.llm import LLMChain
+from langchain_core.prompts import PromptTemplate
+# pip install -U langchain-community
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+# pip install dashscope
+
+from langchain.globals import set_llm_cache
+set_llm_cache(None)
+
+
+def call_llm(question, llm):
+    prompt = PromptTemplate(template="{question}", input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    ans = llm_chain.invoke(question)
+    return ans["text"]
