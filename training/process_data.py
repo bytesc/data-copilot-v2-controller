1077f030fe252abc97a57bac097a5929910c0256
rcy <21010078@mail.ecust.edu.cn>
2024-10-21 00:18:37 +0800

    process data

---
+++ training/process_data.py
@@ -0,0 +1,31 @@
+from utils.read_csv import read_csv_to_list_row
+
+output_file_name = "../output_store/data_log/ask_graph_1.csv"
+
+
+def process_output_list(output_file):
+    output_list = read_csv_to_list_row(output_file)
+    output_list = [[row[1], row[5], row[6], row[7], row[8]] for row in output_list]
+    print(output_list)
+    unique_questions = list(set([row[0] for row in output_list]))
+    print(unique_questions)
+    outcome = {}
+    for question in unique_questions:
+        right = 0
+        wrong = 0
+        retry_list = [0 for _ in range(6)]
+        outputs = [row for row in output_list if row[0] == question]
+        file = ""
+        for result in outputs:
+            if result[1] == "200" or (result[1] == "504" and result[3] != ""):
+                right = right+1
+                retry_list[int(result[2])] = retry_list[int(result[2])]+1
+                file = result[4]
+            else:
+                wrong = wrong + 1
+        outcome[question] = [retry_list, right/(wrong+right), file]
+    print(outcome)
+
+
+if __name__ == "__main__":
+    process_output_list(output_file_name)
--- utils/read_csv.py
+++ utils/read_csv.py
@@ -12,3 +12,14 @@ def read_csv_to_list(file_path):
 
 # cell_list = read_csv_to_list('data.csv')
 # print(cell_list)
+
+
+def read_csv_to_list_row(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        data = [row for row in reader]
+    return data
+
+# Example usage (this won't work here as we don't have access to files in this environment)
+# data = read_csv_to_list("path_to_your_file.csv")
+# print(data)
