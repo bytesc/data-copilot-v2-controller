767019d8fa278a23efe97bb9c209c57a8cc24fe8
rcy <21010078@mail.ecust.edu.cn>
2024-10-21 23:00:44 +0800

    train

--- training/model.py
+++ training/model.py
@@ -8,9 +8,14 @@ from transformers import BertTokenizer, BertModel
 from training.dataset import dataloader_success_rate, train_dataloader_success_rate
 from training.dataset import test_dataloader_success_rate, val_dataloader_success_rate
 
+from utils.write_csv import write_csv_from_list
+
 # 初始化BERT模型和分词器
 tokenizer = BertTokenizer.from_pretrained('D:/IDLE/projects/models/bert-base-multilingual-uncased/')
 bert_model = BertModel.from_pretrained('D:/IDLE/projects/models/bert-base-multilingual-uncased/')
+
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+log_file_name = f'./train_logs/log_{timestamp}.csv'
 
 
 class BertRegressionModel(nn.Module):
@@ -42,37 +47,45 @@ optimizer = Adam(model.parameters(), lr=1e-5)
 
 # 训练模型
 model.train()
+threshold = 0.1
 for epoch in range(10):  # 训练10个epoch
     train_loss = 0.0
+    correct_predictions_train = 0
+    total_samples_train = 0
+    sum_abs_error_train = 0.0  # 累加绝对误差
+
     for batch in train_dataloader_success_rate:
         x, y = batch
         y = y.float().to(device)  # Convert y to float and then move to device
+        total_samples_train += len(y)
 
-        # 编码文本数据
         inputs = tokenizer(x[0], return_tensors="pt", padding=True, truncation=True, max_length=512)
         input_ids = inputs['input_ids'].to(device)
         attention_mask = inputs['attention_mask'].to(device)
 
-        # 前向传播
         outputs = model(input_ids, attention_mask)
-        loss = loss_function(outputs.float(), y.unsqueeze(1))  # Cast outputs to float
+        loss = loss_function(outputs.float(), y.unsqueeze(1))
 
-        # 反向传播
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         train_loss += loss.item()
 
-    # 验证模型
-    val_loss = 0.0
+        abs_error = torch.abs(outputs.flatten() - y)
+        correct_predictions_train += (abs_error < threshold).sum().item()
+        sum_abs_error_train += abs_error.sum().item()  # 累加绝对误差
+
     model.eval()
-    total_abs_error = 0.0
-    total_samples = 0
+    correct_predictions_val = 0
+    sum_abs_error_val = 0.0  # 累加绝对误差
+    total_samples_val = 0
+
     with torch.no_grad():
-        for batch in test_dataloader_success_rate:
+        for batch in val_dataloader_success_rate:
             x, y = batch
             y = y.to(device)
+            total_samples_val += len(y)
 
             inputs = tokenizer(x[0], return_tensors="pt", padding=True, truncation=True, max_length=512)
             input_ids = inputs['input_ids'].to(device)
@@ -81,31 +94,51 @@ for epoch in range(10):  # 训练10个epoch
             outputs = model(input_ids, attention_mask)
             predicted = outputs.flatten()
 
-            # 计算预测值和实际值之间的绝对误差
             abs_error = torch.abs(predicted - y)
-            total_abs_error += abs_error.sum().item()
-            total_samples += len(y)
+            correct_predictions_val += (abs_error < threshold).sum().item()
+            sum_abs_error_val += abs_error.sum().item()  # 累加绝对误差
 
-    # 计算平均绝对误差
-    mean_abs_error = total_abs_error / total_samples
-    print(f'Epoch: {epoch}, Train Loss: {train_loss / len(train_dataloader_success_rate)},'
-          f' Val Loss: {val_loss / len(val_dataloader_success_rate)},'
-          f' Mean Absolute Error: {mean_abs_error}')
+    val_accuracy = correct_predictions_val / total_samples_val
+    val_avg_abs_error = sum_abs_error_val / total_samples_val
+    train_accuracy = correct_predictions_train / total_samples_train
+    train_avg_abs_error = sum_abs_error_train / total_samples_train
+    print(
+        f'Epoch: {epoch}, Train Loss: {train_loss / len(train_dataloader_success_rate)}, '
+        f' Train Accuracy: {train_accuracy}, Train Average Error: {train_avg_abs_error}, '
+        f' Validation Accuracy: {val_accuracy}, Validation Average Error: {val_avg_abs_error}')
+    write_csv_from_list(log_file_name, [epoch, train_loss / len(train_dataloader_success_rate),
+                                        train_accuracy, train_avg_abs_error,
+                                        epoch, val_accuracy, val_avg_abs_error])
 
 # 测试模型
 model.eval()
+correct_predictions = 0
+sum_abs_error_test = 0.0  # 累加绝对误差
+total_samples_test = 0
+
 with torch.no_grad():
     for batch in test_dataloader_success_rate:
         x, y = batch
         y = y.to(device)
+        total_samples_test += len(y)
 
         inputs = tokenizer(x[0], return_tensors="pt", padding=True, truncation=True, max_length=512)
         input_ids = inputs['input_ids'].to(device)
         attention_mask = inputs['attention_mask'].to(device)
 
         outputs = model(input_ids, attention_mask)
+        predicted = outputs.flatten()
+
+        abs_error = torch.abs(predicted - y)
+        correct_predictions += (abs_error < threshold).sum().item()
+        sum_abs_error_test += abs_error.sum().item()  # 累加绝对误差
+
         print(f'Predicted: {outputs}, Actual: {y}')
 
+    test_accuracy = correct_predictions / total_samples_test
+    test_avg_abs_error = sum_abs_error_test / total_samples_test  # 计算平均绝对误差
+    print(f'Test Accuracy: {test_accuracy}, Test Average Absolute Error: {test_accuracy}')
+    write_csv_from_list(log_file_name, [test_accuracy, test_accuracy])
 
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-torch.save(model.state_dict(), f'model_{timestamp}.pth')
+torch.save(model.state_dict(), f'./saves/model_{timestamp}.pth')
