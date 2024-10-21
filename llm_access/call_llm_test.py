1f65211718cb88df9f4c070b373059ccebb776ac
rcy <21010078@mail.ecust.edu.cn>
2024-10-21 23:45:48 +0800

    update pip

--- llm_access/call_llm_test.py
+++ llm_access/call_llm_test.py
@@ -6,11 +6,12 @@ from langchain.prompts import PromptTemplate
 # pip install dashscope
 
 from langchain.globals import set_llm_cache
+
 set_llm_cache(None)
 
 
 def call_llm(question, llm):
     prompt = PromptTemplate(template="{question}", input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm_chain = prompt | llm
     ans = llm_chain.invoke(question)
-    return ans["text"]
+    return ans
